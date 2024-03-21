03feee73eaa7545a2e52fc9cdcb47d81fbde6fc2
Danny Sarco <dsarco@gmail.com>
2024-03-21 13:58:11 -0400

    Squashed bug causing unwanted pop-up on page load in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+/**
+ * Bug has been squashed into a log.
+ */
 function bug1() {
-  alert("Fix me! I am a bug");
+  console.log("I'm a squashed bug!");
 }
 
 document.addEventListener("DOMContentLoaded", bug1);
