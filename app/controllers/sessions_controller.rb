5f6156329ec9611b9657507a11c8124ed265e1bd
Danny Sarco <dsarco@gmail.com>
2024-03-21 14:23:26 -0400

    Fixed logout session handling by correcting key typo and implementing reset_session for security.
    Added detailed comments throughout the `SessionsController`, especially in the `create` and `destroy` actions, to improve code clarity and document the purpose and importance of each step in the login and logout processes.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
 
+  # Skipping authentication checks for login actions to prevent redirect loops
   skip_before_action :authenticated?, only: [:login, :create]
 
-  # Login
+  # Handles the login process
   def create
+    # Find the user by username submitted through the login form
     @user = User.find_by(username: params[:username])
     @current_user = @user
 
+    # Authenticate the user. If successful, store the user ID in the session to keep them logged in.
     if !!@current_user && @current_user.authenticate(params[:password])
       session[:user_id] = @current_user.id
       redirect_to root_path
@@ -14,13 +17,13 @@ class SessionsController < ApplicationController
       message = "Username or password invalid"
       redirect_to login_path, notice: message
     end
-
   end
 
-  # Logout
+  # Handles the logout process
   def destroy
-    @current_user = nil
-    session.delete(:user_iid)
+    # Resetting the entire session for a clean logout, ensuring no leftover session data
+    reset_session
+    # Redirecting the user to the login page with a success message
     redirect_to login_path, notice: "Successfully logged out"
   end
 
